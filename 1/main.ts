d465f6d0ec99fafb909de70f879411bcb74c18e2
djangofs <djangoshelton@gmail.com>
2022-12-02 09:29:41 +0000

    part 2

--- 1/main.ts
+++ 1/main.ts
@@ -8,8 +8,17 @@ const totalCalories = splitData.map((elf) =>
   elf.reduce((a, b) => Number(a) + Number(b), 0)
 );
 
+// Part 1
+
 const maxCalories = Math.max(...totalCalories);
 
 console.log(maxCalories);
-
 // 71023
+
+// Part 2
+
+const sortedCalories = totalCalories.sort((a, b) => b - a);
+const top3Sum = sortedCalories.slice(0, 3).reduce((a, b) => a + b, 0);
+
+console.log(top3Sum);
+// 206289
